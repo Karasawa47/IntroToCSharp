03baf242b2db66c3109bf2763034a1492168a29a
Karasawa47 <karasawa47@gmail.com>
2014-06-26 08:09:14 -0700

    added HelloWorld
    Added HelloWorld project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            var inputReadLine=Console.ReadLine();
+            Console.WriteLine(inputReadLine);
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
